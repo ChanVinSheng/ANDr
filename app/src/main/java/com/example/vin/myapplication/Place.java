6b6f295e0ba39c8fa3f315524ad02bcfd82977f0
Vin <chanvs-wa16@student.tarc.edu.my>
2019-01-02 16:30:46 +0800

    df

---
+++ app/src/main/java/com/example/vin/myapplication/Place.java
@@ -0,0 +1,46 @@
+package com.example.vin.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.example.vin.myapplication.MAP.MapsActivity;
+import com.example.vin.myapplication.NearbyMap.navigationMenu;
+
+public class Place extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_test,container,false);
+
+
+        ImageView myButton = (ImageView) view.findViewById(R.id.placenearby);
+        myButton.setClickable(true);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), navigationMenu.class));
+            }
+        });
+
+        ImageView myButton2 = (ImageView) view.findViewById(R.id.placemap);
+        myButton2.setClickable(true);
+        myButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), MapsActivity.class));
+            }
+        });
+
+
+
+        return view;
+    }
+}
